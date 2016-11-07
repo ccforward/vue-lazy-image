54fe5bda2c66e467acf98c5b48ac5ab9d852731f
cc <ccking@foxmail.com>
2016-11-07 20:11:14 +0800

    publish to npm

---
+++ webpack.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
